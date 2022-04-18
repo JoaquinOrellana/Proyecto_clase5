d09e7d4dfff541374669db4f7c91d625900bb6e7
Joaquin Orellana <joaquin.orellana@pucp.edu.pe>
2022-04-18 10:13:05 -0500

    Initial commit

---
+++ src/main/java/com/example/proyecto_clase5/ProyectoClase5Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_clase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoClase5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_clase5/ProyectoClase5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_clase5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoClase5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
