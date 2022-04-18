264cd6dec7bb9e81af6ff1563c32e4f2452bd397
dianasr <sologurenr.d@pucp.edu.pe>
2022-04-18 10:43:15 -0500

    Archivos

---
+++ src/main/java/com/example/proyecto_clase5/controller/HistoryController.java
@@ -0,0 +1,18 @@
+package com.example.proyecto_clase5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping(value = "/history")
+public class HistoryController {
+
+    @GetMapping(value = {"","/"})
+    public String historialEmpleado(){
+        //COMPLETAR
+    }
+
+
+
+}
---
+++ src/main/java/com/example/proyecto_clase5/entity/Employees.java
@@ -0,0 +1,11 @@
+package com.example.proyecto_clase5.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="employees")
+public class Employees {
+
+    //COMPLETAR
+}
